969e2361100c9d690297626e23e77d3ed5895dda
YannLeo <1070032777@qq.com>
2021-09-06 14:29:53 +0800

    在config文件夹内添加新的json文件，可同时给多个人打卡与晚点名

--- main.py
+++ main.py
@@ -3,24 +3,21 @@ import os
 from apscheduler.schedulers.blocking import BlockingScheduler
 import evening
 import morning
-import argparse
 import json
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--config', type=str, default="config.json")
-
-def getINFO():
-    args = parser.parse_args()
-    with open(args.config) as f:
+def getINFO(path_config):
+    with open(path_config) as f:
         info = json.load(f)
     return info
 
 if __name__ == '__main__':
-    info = getINFO()
     scheduler = BlockingScheduler()
-    time_morning = info['time']['morning']
-    time_evening = info['time']['evening']
-    scheduler.add_job(morning.morning, 'cron', hour=time_morning['hour'], minute=time_morning['minute'], args=[info])
-    scheduler.add_job(evening.evening, 'cron', hour=time_evening['hour'], minute=time_evening['minute'], args=[info])
+    for root, dirs, files in os.walk('/home/yl/CodeAndData/UESTC_student_health_report/config'):
+        for f in files:
+            info = getINFO(root + '/' + f)
+            # print(f, info)
+            time_morning = info['time']['morning']
+            time_evening = info['time']['evening']
+            scheduler.add_job(morning.morning, 'cron', hour=time_morning['hour'], minute=time_morning['minute'], args=[info])
+            scheduler.add_job(evening.evening, 'cron', hour=time_evening['hour'], minute=time_evening['minute'], args=[info])
     scheduler.start()
-    
